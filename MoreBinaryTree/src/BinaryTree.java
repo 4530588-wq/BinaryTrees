3bbb5426374fd37d96b7fb4c150e2f15b908ec47
Lithemba <4530588@myuwc.ac.za>
2026-04-21 03:33:53 +0200

    Last commit for the day

--- MoreBinaryTree/src/BinaryTree.java
+++ MoreBinaryTree/src/BinaryTree.java
@@ -7,4 +7,24 @@ public class BinaryTree<B> {
     public BinaryTree(BinaryTreeNode<B> root){
         this.root = root;
     }
+
+    public BinaryTreeNode<B> getRoot(){
+        return root;
+    }
+    public void setRoot(BinaryTreeNode<B> root){
+        this.root = root;
+    }
+
+    public int getHeight(BinaryTreeNode<B> node){
+        if(node == null) return 0;
+
+        int leftHeight = getHeight(node.getLeft());
+        int rightHeight = getHeight(node.getRight());
+
+        return Math.max(leftHeight,rightHeight) +1;
+    }
+
+    public int countLevel(BinaryTreeNode<Integer> node, int level){
+        
+    }
 }
